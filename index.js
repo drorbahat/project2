c8a181bc30012f7d6d765ffcf6b63a545b34fdae
dror bahat <bahat.dror@gmail.com>
2022-01-09 13:53:56 +0200

    show more button working

--- index.js
+++ index.js
@@ -18,24 +18,60 @@ $(() => {
     const displayDataToGrid = (first100Arr) => {
         const cardContainer = $('#cards-container')
         let cards = ``
+        let idNumCounter = 0
         first100Arr.forEach(element => {
+            idNumCounter++
             cards += `
             <div class="col-sm-4 ">
-            <div class="card shadow">
-            <div class="card-body">
-            <div>
-            <h3 class="card-title">${element.symbol}</h3>
-            </div>
-            <p class="card-text">${element.name}</p>
-            <button class="btn btn-primary">show more</button>
-            </div>
-            </div>
+                <div class="card shadow">
+                    <div class="card-body">
+                        <div>
+                            <h3 class="card-title">${element.symbol}</h3>
+                        </div>
+                        <p class="card-text">${element.name}</p>
+                        <div class="custom-control custom-switch">
+                            <input type="checkbox" class="custom-control-input" id="switch${idNumCounter}" name="example">
+                            <label class="custom-control-label" for="switch${idNumCounter}"></label>
+                        </div>
+                        <br>
+                        <button id="${element.id}" type="button" class="btn btn-primary more-info-button">show more</button>
+                    </div>
+                </div>
             </div>
             `
         });
 
         cardContainer.append(cards)
+        moreInfoFunction()
     }
+
+    const moreInfoFunction = () => {
+        $(".more-info-button").click(function () {
+            let cardId = $(this).attr("id")
+            $.ajax({
+                url: `https://api.coingecko.com/api/v3/coins/${cardId}`,
+                success: function (data) {
+                    appendDataContainer(data)
+                }
+            });
+
+            const appendDataContainer = (data) => {
+                let moreInfo = `
+                <div class="more-data-container">
+                        <p class="card-text">${data.market_data.current_price.usd}$</p>
+                        <p class="card-text">${data.market_data.current_price.eur}€</p>
+                        <p class="card-text">${data.market_data.current_price.ils}₪</p>
+                        <img src="${data.image.small}" alt="${data.symbol} img">
+                    </div>
+                `
+                $(this).parent().append(moreInfo)
+            }
+
+        })
+    }
+
+
+
 
     $('#home-button').click(function (e) {
         $('#spinner-container').show();
@@ -43,6 +79,94 @@ $(() => {
         $('#spinner-container').hide();
     });
 
-    
-})
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
